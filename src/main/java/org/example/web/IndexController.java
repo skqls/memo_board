113766d17d65fe14a49d1a5d27bd57fd2339dc97
skqls <kitchin105@gmail.com>
2023-03-11 19:43:23 +0900

    login function 1.5

--- src/main/java/org/example/web/IndexController.java
+++ src/main/java/org/example/web/IndexController.java
@@ -1,6 +1,7 @@
 package org.example.web;
 
 import lombok.RequiredArgsConstructor;
+import org.example.config.auth.LoginUser;
 import org.example.config.auth.dto.SessionUser;
 import org.example.service.PostsService;
 import org.example.web.dto.PostsResponseDto;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
